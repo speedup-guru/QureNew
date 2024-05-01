81390d791dd013cd58fe69d3c5fe964600123bd7
nicolanda <nicolaslandazabal@hotmail.com>
2024-05-01 15:31:49 -0500

    working on text renderin breadCrumbs

--- assets/endrock.js
+++ assets/endrock.js
@@ -227,10 +227,13 @@ document.addEventListener('DOMContentLoaded', function() {
       this.btnNext = document.getElementById('landing-next');
       this.bntTest = document.getElementById('landing-test');
       this.btnSubmit = document.getElementById('landing-submit');
+      this.breadCrumbsItems = document.querySelectorAll('.landing-breadcrumbs-item');
+      this.breadCrumbsButtons = document.querySelectorAll('.landing-breadcrumbs-item__btn');
       this.selectedInputValue = null;
       this.selectedInputValueStep4 = null;
       this.selectedInputValueJson = null;
       this.selectedInputValueJsonStep4 = null;
+      this.selectedInputStep2Title = null;
       this.indexPanel = 0;
 
       this.hidePanels();
@@ -241,10 +244,19 @@ document.addEventListener('DOMContentLoaded', function() {
       this.updateVisibilityAndValueFourthStep();
       this.updateSelectedInputValueJsonStep4();
       this.updateButtons();
+      this.updateVisibilityBreadcrumb();
+
+      // assing
+      const firstStep2RadioButton = document.querySelector('#step2-select input[type="radio"]:checked');
+      if (firstStep2RadioButton) {
+        this.selectedInputStep2Title = firstStep2RadioButton.dataset.title;
+      }
 
       const radioButtonsStep2 = document.querySelectorAll('#step2-select input[type="radio"]');
       radioButtonsStep2.forEach(button => {
         button.addEventListener('change', () => {
+          const title = button.dataset.title;
+          this.selectedInputStep2Title = title;
           this.updateVisibilityFirstStep();
           this.updateSelectedInputValueJson();
           this.updateVisibilityAndValueFourthStep();
@@ -261,6 +273,7 @@ document.addEventListener('DOMContentLoaded', function() {
           this.updateSelectedInputValue();
           this.renderElementsStep3();
           this.updateSelectedInputValueJson();
+          this.updateVisibilityBreadcrumb();
         });
       });
 
@@ -276,16 +289,17 @@ document.addEventListener('DOMContentLoaded', function() {
       // buttons
       this.btnNext.addEventListener('click', () =>{
         this.nextPanel();
+        
       });
 
       this.btnBack.addEventListener('click', () =>{
         this.backPanel();
       });
 
-      this.bntTest.addEventListener('click', () =>{
-        console.log('test');
-        console.log('Value of selected radio button):', this.selectedInputValueJson);
-        console.log('Value of selected radio button 4):', this.selectedInputValueJsonStep4);
+      this.breadCrumbsButtons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+          this.showPanelBread(index);
+        });
       });
 
       this.btnSubmit.addEventListener('click', () =>{
@@ -297,33 +311,50 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
       // functions
-      hidePanels() {
-        this.panels.forEach(panel =>{
-          panel.classList.add('panel-hidden');
-        });
+    hidePanels() {
+      this.panels.forEach(panel =>{
+        panel.classList.add('panel-hidden');
+      });
+    }
+
+    showActualPanel(index) {
+      this.panels[index].classList.remove('panel-hidden');
+    }
+
+    hidePanel(index) {
+      this.panels[index].classList.add('panel-hidden');
+    }
+
+    nextPanel(){
+      if (this.indexPanel < this.panels.length - 1) {
+        this.breadCrumbsItems[this.indexPanel].classList.remove('active-breadcrumb');
+
+        this.hidePanel(this.indexPanel);
+        this.indexPanel++;
+        this.showActualPanel(this.indexPanel);
+        this.updateButtons();
+        this.breadCrumbsButtons[this.indexPanel].removeAttribute('disabled');
+        this.breadCrumbsItems[this.indexPanel].classList.add('active-breadcrumb');
       }
+    }
 
-      showActualPanel(index) {
-        this.panels[index].classList.remove('panel-hidden');
+    showPanelBread(index){
+      if(index >= 0 && index < this.panels.length){
+        this.breadCrumbsItems[this.indexPanel].classList.remove('active-breadcrumb');
+        this.hidePanels();
+        this.showActualPanel(index);
+        this.breadCrumbsButtons.forEach((button, i) => {
+          if (i > index) {
+            button.setAttribute('disabled', 'disabled');
+          } else {
+            button.removeAttribute('disabled');
+          }
+        })
+        this.breadCrumbsItems[index].classList.add('active-breadcrumb');
+        this.indexPanel = index;
+        this.updateButtons();
       }
-
-      hidePanel(index) {
-        this.panels[index].classList.add('panel-hidden');
-      }
-
-      nextPanel(){
-        // console.log('Position:', this.selectedInputValueJson.position);
-        // console.log('Index:', this.indexPanel);
-        // console.log('Length:', this.panels.length);
-        // console.log('Selected input value:', this.selectedInputValueJson);
-        // console.log('Selected input value:', this.selectedInputValue.dataset);
-
-        if (this.indexPanel < this.panels.length - 1) {
-          this.hidePanel(this.indexPanel);
-          this.indexPanel++;
-          this.showActualPanel(this.indexPanel);
-          this.updateButtons();
-        }
+     
     }
       
     updateButtons(){
@@ -350,6 +381,9 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     backPanel(){
+      this.breadCrumbsItems[this.indexPanel].classList.remove('active-breadcrumb');
+
+      this.breadCrumbsButtons[this.indexPanel].setAttribute('disabled', 'disabled');
       this.hidePanel(this.indexPanel);
       this.indexPanel--;
       if(this.indexPanel < 0){
@@ -357,6 +391,8 @@ document.addEventListener('DOMContentLoaded', function() {
       }
       this.showActualPanel(this.indexPanel);
       this.updateButtons();
+      this.breadCrumbsItems[this.indexPanel].classList.add('active-breadcrumb');
+      
     }     
 
     syncRadioButtons(value) {
@@ -416,12 +452,12 @@ document.addEventListener('DOMContentLoaded', function() {
       // console.log('Selected input value 4:', this.selectedInputValueStep4);
     }
 
-      
+    
+    // ? tratar de unir estas dos funciones en una sola
 
     updateVisibilityFirstStep(){
       const secondRadioButtons = document.querySelector('#step2-select input[name="step2"]:checked');
       const selectOption = secondRadioButtons.value;
-      console.log('selected 2', selectOption);
 
       const allInputs = document.querySelectorAll('.inputs-products1, .inputs-products2, .inputs-products3');
       allInputs.forEach(input => {
@@ -433,15 +469,12 @@ document.addEventListener('DOMContentLoaded', function() {
       const selectedRadioButton = selectedInputs.querySelector('input[type="radio"]:checked');
       this.selectedInputValue = selectedRadioButton ? selectedRadioButton.value : null;
       this.updateSelectedInputValueJson();
-      // console.log('Selected input value 1:', this.selectedInputValue);
-      // console.log('Selected input value 2:', this.selectedInputValueJson);
     }
 
     updateVisibilityAndValueFourthStep(){
       const secondRadioButtons = document.querySelector('#step2-select input[name="step2"]:checked');
       const selectOption = secondRadioButtons ?  secondRadioButtons.dataset.step4Product : null;
-      // console.log('selected 2 for 4step', selectOption);
-      // Ocultar todos los elementos del cuarto paso
+
       const allStep4Inputs = document.querySelectorAll('.inputs-products4, .inputs-products5, .inputs-products6');
       allStep4Inputs.forEach(input => {
         input.classList.remove('show-inputs');
@@ -453,14 +486,11 @@ document.addEventListener('DOMContentLoaded', function() {
       const selectedRadioButtonStep4 = selectedInputsStep4.querySelector('input[type="radio"]:checked');
       this.selectedInputValueStep4 = selectedRadioButtonStep4 ? selectedRadioButtonStep4.value : null;
       this.updateSelectedInputValueJsonStep4();
-      // console.log('Selected input value 4:', this.selectedInputValueStep4);
 
     }
+    // ? end
 
     renderElementsStep3(){
-      // const selectedInputJson = JSON.parse(this.selectedInputValue);
-      // console.log('Selected input value:', selectedInputJson);
-
       const step3PriceNormal = document.getElementById('step3-price-normal');
       const step3PriceQuota = document.getElementById('step3-price-quota');
 
@@ -474,6 +504,17 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
 
+    updateVisibilityBreadcrumb(){
+      const breadCrumb4 = document.querySelector('.breadStep-4');
+      console.log('Selected input value in bread fx:', this.selectedInputValueJson);
+      console.log('bread', breadCrumb4)
+      if(this.selectedInputValueJson.position !== 1){
+        breadCrumb4.classList.add('hidden');
+      } else {
+        breadCrumb4.classList.remove('hidden');
+      }
+    }
+
 
     updateAnchorValue(){
       console.log('Add to cart');
@@ -483,28 +524,25 @@ document.addEventListener('DOMContentLoaded', function() {
       console.log('productIdStep1', productIdStep1);
       console.log('productIdStep4', productIdStep4);
       let currentHref = anchorID.getAttribute('href');
-
+      let selectedProductId = productIdStep1.id;
 
 
       // Obtener el producto correspondiente según la posición
-      const selectedProductId = (productIdStep1.position === 1 && productIdStep4.position === 1) 
-      ? productIdStep4.id 
-      : (productIdStep1.position === 1 && productIdStep4.position !== 1) 
-        ? productIdStep1.id 
-        : productIdStep1.id;
+      // const selectedProductId = (productIdStep1.position === 1 && productIdStep4.position === 1) 
+      // ? productIdStep4.id 
+      // : (productIdStep1.position === 1 && productIdStep4.position !== 1) 
+      //   ? productIdStep1.id 
+      //   : productIdStep1.id;
 
-        console.log(currentHref, selectedProductId)
-      // if (currentHref.includes(`?id`)) {
-      //   currentHref = currentHref.replace(`?id=${selectedProductId}&quantity=1`, '');
-      // } else {
-      //   currentHref += `?id=${selectedProductId}&quantity=1`;
-      // }
+      if (productIdStep1.position === 1) {
+        if (productIdStep4.position === 1) {
+            selectedProductId = productIdStep4.id;
+        } else {
+            selectedProductId = productIdStep1.id;
+        }
+      }
 
-
-      // anchorID.setAttribute('href', currentHref);
-      // console.log('anchor', anchorID);
-
-
+      console.log(currentHref, selectedProductId)
       const href = currentHref.split('add')[0]
       currentHref = `${href}add?id=${selectedProductId}&quantity=1`
       console.log('currentHref', currentHref);
