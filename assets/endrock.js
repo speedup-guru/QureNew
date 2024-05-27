d278c296cd8b90734057d668e4375ecf37f3ee48
nicolanda <nicolaslandazabal@hotmail.com>
2024-05-27 16:36:47 -0500

    document js function, a fix on css

--- assets/endrock.js
+++ assets/endrock.js
@@ -770,14 +770,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
   /* QR- Shopify 2.0 Features Adjustments  */
 
-  const greenBtn = document.querySelector('#green-product-sticky-btn');
+  // Select the green product sticky button element
+  const greenProductStickyBtn = document.querySelector('#green-product-sticky-btn');
+
+  /**
+   * Centers the given button horizontally within the window.
+   * @param {HTMLElement} btn - The button element to be centered.
+   */
   const centerGreenBtn = (btn) => {
+    // Calculate the total left offset to center the button
     const totalLeft = (window.innerWidth - btn.offsetWidth) / 2;
+    // Set the calculated left offset as the button's left CSS property
     btn.style.left = `${totalLeft}px`;
   };
-  if(greenBtn){
-    centerGreenBtn(greenBtn);
-    window.onresize = () => centerGreenBtn(greenBtn);
+
+  // Check if the green product sticky button element exists
+  if (greenProductStickyBtn) {
+    // Center the button initially
+    centerGreenBtn(greenProductStickyBtn);
+    // Re-center the button when the window is resized
+    window.onresize = () => centerGreenBtn(greenProductStickyBtn);
   }
 
   /* end QR- Shopify 2.0 Features Adjustments  */
