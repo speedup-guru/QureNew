c59c091e59f7c489651b67f88f103acc03410d8b
speedup-guru <paul@speedup.guru>
2024-06-13 18:00:14 +0400

    https://app.clickup.com/t/8694traba

--- assets/custom.js
+++ assets/custom.js
@@ -175,11 +175,34 @@ document.addEventListener('DOMContentLoaded', function() {
             triggerClick('.take-care');
         });
     }
+
+    var swiper = new Swiper(".combat_slider", {
+        slidesPerView: 3.6,
+        grid: {
+            rows: 1,
+            fill: "row",
+        },
+        spaceBetween: 20,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            556: {
+                slidesPerView: 5.3,
+            },
+            768: {
+                slidesPerView: 3,
+                grid: {
+                    rows: 3,
+                    fill: "row",
+                }
+            },
+    
+        },
+    });
+
 });
-
-
-
-
 
 function triggerClick(selector) {
     var event = new Event('click', {
